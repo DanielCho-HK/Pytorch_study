fe8b4113875154e22435d719e3420460b3793779
Daniel TSAO <81144207+DanielCho-HK@users.noreply.github.com>
2021-11-02 17:00:10 +0800

    Add files via upload

---
+++ pytorch_study/dataset_transform/main.py
@@ -0,0 +1,17 @@
+import torchvision
+from torch.utils.tensorboard import SummaryWriter
+
+dataset_transform = torchvision.transforms.Compose([
+    torchvision.transforms.ToTensor()
+])
+
+
+train_set = torchvision.datasets.CIFAR10(root='./dataset', train=True, transform=dataset_transform, download=True)
+test_set = torchvision.datasets.CIFAR10(root='./dataset', train=False, transform=dataset_transform, download=True)
+
+writer = SummaryWriter('p10')
+for i in range(100):
+    img, target = test_set[i]
+    writer.add_image("test_set", img, i)
+
+writer.close()
